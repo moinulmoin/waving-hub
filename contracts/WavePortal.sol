4d55f352e1594f33954d0076f1f5389b5b70bb7d
Moinul Moin <moinulmoin101@gmail.com>
2022-11-23 01:10:01 +0600

    first sm

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.17;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("Yo, I am a smart contract");
+    }
+}
